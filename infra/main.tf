d506f8d1fffa757d194c6d58d118b4b98381255c
Anton <92355198+b100to@users.noreply.github.com>
2023-03-31 19:28:49 +0900

    WIP

--- infra/main.tf
+++ infra/main.tf
@@ -18,14 +18,13 @@ module "vpc" {
 module "sg_app" {
   source     = "cloudposse/security-group/aws"
   version    = "2.0.1"
-  attributes = ["primary"]
+  attributes = [local.config.name]
 
   # Allow unlimited egress
   allow_all_egress = true
 
   rules = [
     {
-      key         = "HTTP"
       type        = "ingress"
       from_port   = 8000
       to_port     = 8000
@@ -34,7 +33,6 @@ module "sg_app" {
       self        = true
     }
   ]
-
   vpc_id = module.vpc.vpc_id
   tags   = local.config.tags
 }
@@ -51,17 +49,30 @@ resource "aws_ecs_cluster" "default" {
   tags = local.config.tags
 }
 
-module "container_definition" {
+module "container_definition_app" {
   source                       = "cloudposse/ecs-container-definition/aws"
   version                      = "0.58.2"
-  container_name               = local.config.container_name
-  container_image              = local.config.container_image
-  container_memory             = local.config.container_memory
-  container_memory_reservation = local.config.container_memory_reservation
-  container_cpu                = local.config.container_cpu
-  essential                    = local.config.container_essential
-  environment                  = local.config.container_environment
-  port_mappings                = local.config.container_port_mappings
+  container_name               = local.config.container_app_name
+  container_image              = local.config.container_app_image
+  container_memory             = local.config.container_app_memory
+  container_memory_reservation = local.config.container_app_memory_reservation
+  container_cpu                = local.config.container_app_cpu
+  essential                    = local.config.container_app_essential
+  environment                  = local.config.container_app_environment
+  port_mappings                = local.config.container_app_port_mappings
+}
+
+module "container_definition_postgresql" {
+  source                       = "cloudposse/ecs-container-definition/aws"
+  version                      = "0.58.2"
+  container_name               = local.config.container_postgresql_name
+  container_image              = local.config.container_postgresql_image
+  container_memory             = local.config.container_postgresql_memory
+  container_memory_reservation = local.config.container_postgresql_memory_reservation
+  container_cpu                = local.config.container_postgresql_cpu
+  essential                    = local.config.container_postgresql_essential
+  environment                  = local.config.container_postgresql_environment
+  port_mappings                = local.config.container_postgresql_port_mappings
 }
 
 module "ecs_alb_service_task" {
@@ -70,16 +81,16 @@ module "ecs_alb_service_task" {
   name               = local.config.name
   alb_security_group = module.vpc.default_security_group_id
   container_definition_json = jsonencode([
-    module.container_definition.json_map_object,
+    module.container_definition_app.json_map_object,
+    module.container_definition_postgresql.json_map_object
   ])
   ecs_cluster_arn                = aws_ecs_cluster.default.arn
   launch_type                    = local.config.ecs_launch_type
   vpc_id                         = module.vpc.vpc_id
   security_group_ids             = [module.vpc.default_security_group_id]
-  subnet_ids                     = module.vpc.public_subnets
+  subnet_ids                     = module.vpc.private_subnets
   ignore_changes_task_definition = local.config.ignore_changes_task_definition
   network_mode                   = local.config.network_mode
-  assign_public_ip               = local.config.assign_public_ip
   propagate_tags                 = local.config.propagate_tags
   desired_count                  = local.config.desired_count
   task_memory                    = local.config.task_memory
